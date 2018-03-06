31c0f7b4ef4e0e0e880cd72732e0e6ed70fbd4af
Jesse Billard <jessebillard@mac.com>
2018-03-06 19:50:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
     if(typeof array[i] === 'function') {
-       return findFn()
+       findFn()
     } else {
        return null
       }
