049250b33356ec59cd6bbfde83eaf763f7852b77
Jesse Billard <jessebillard@mac.com>
2018-03-06 19:49:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+//in bidness
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === stopValue) {
+      break;
+    } array[i] = changeValue
+  } return array
+}
+
+//vamanos muchachos
+function keepGoing(array, changeValue, skipValue) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === skipValue) {
+      continue;
+    } array[i] = changeValue
+  } return array
+}
+
+//hokey dokey
+function findBy(array, findFn) {
+  for(let i = 0; i < array.length; i++) {
+    if(typeof findFn(array[i]) === 'number') {
+       return array[i]
+    } else {
+       return null
+      }
+   }
+  
+}
